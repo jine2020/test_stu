cef5c02cab0389ef7a4847d9a5b0c2674691139e
jine <65452578+jine2020@users.noreply.github.com>
2020-09-01 18:46:18 +0800

    Update test_selenium.py

--- selenium/test_selenium.py
+++ selenium/test_selenium.py
@@ -16,4 +16,4 @@ class TestHogwards:
     def test_hogwards(self):
         self.driver.get('https://www.baidu.com')
         sleep(2)
-        self.driver.find_element(By.XPATH,'//*[@title="北京西城区某单位出现33人集中发热，排除新冠肺炎，但与空调有关"]').click()
+        self.driver.find_element(By.XPATH,'//*[@title="//*[@id="hotsearch-content-wrapper"]/li[1]/a/span[2]"]').click()
